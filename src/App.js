944030445b19d4040ea193f6f4182d0e29003a8e
Michael Riccardi <michaelriccardi@ufl.edu>
2017-08-27 23:12:32 -0400

    Basic edit card in-place functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-/*import logo from './logo.svg';*/
 import './App.css';
 import jQuery from 'jquery';
-import Modal from 'react-modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Moment from 'moment';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
-import Authentication from './authentication.js'
+import Authentication from './authentication.js';
+import Textarea from 'react-textarea-autosize';
+
+//import './react-datetime.css';
+//var Datetime = require('react-datetime');
+
 
 class Icon extends Component {
 
@@ -60,66 +63,238 @@ class Stat extends Component {
     }
 
     render() {
-        return (
-            <span className={this.state.classNames}>
-                <Icon name={this.props.icon} />
-                {this.props.value}
-            </span>
-        );
+    	if(this.props.editing) {
+
+    		var editField;
+
+    		switch(this.props.type)
+    		{
+    			case 'text':
+    				editField = <span>
+    								<input type="text" name="estimated" defaultValue={this.props.value} />
+    								<br />
+    							</span>
+				break;
+
+    			case 'date-time':
+    				var date = (this.props.due) ? Moment(this.props.due).format('YYYY-MM-DD') : "";
+    				var time = (this.props.due) ? Moment(this.props.due).format('HH:mm:') + "00" : "";
+
+    				editField = <span>
+    								<input type="date" name="dueDate" defaultValue={date}  />
+    								<input type="time" name="dueTime" defaultValue={time} />
+    								<br /><br />
+    							</span>
+    			break;
+
+    			case 'event':
+    				var scheduledDate = (this.props.scheduledStart) ? this.props.scheduledStart.format("YYYY-MM-DD") : null;
+    				var start = (this.props.scheduledStart) ? this.props.scheduledStart.format("HH:mm:00") : null;
+    				var end = (this.props.scheduledEnd) ? this.props.scheduledEnd.format("HH:mm:00") : null;
+
+    				editField = <span>
+    								<input type="date" name="scheduledDate" defaultValue={scheduledDate} /><br />
+    								from <input type="time" name="scheduledTimeStart" defaultValue={start} />
+    								to <input type="time" name="scheduledTimeEnd" defaultValue={end} />
+								</span>
+    			break;
+
+    			default:
+    				editField = "???"
+    			break;
+
+    		}
+
+    		return ( 
+    			<div className="form-group">
+	                <Icon name={this.props.icon} />
+	                {editField}
+	            </div>
+    		);
+    	}
+    	else {
+	        return (
+	            <span className={this.state.classNames}>
+	                <Icon name={this.props.icon} />
+	                {this.props.value}
+	            </span>
+	        );
+	    }
     }
 }
     
 class Card extends Component {
 
-    state = {
-        cycle: Math.floor(Math.random()*60)+"min",
-        due: (this.props.due) ? Moment(this.props.due).format("ddd M/D h:mma") : "TBD",
-        estimated: (this.props.description.indexOf("{est=") > -1) ?
-            this.props.description.substring( this.props.description.indexOf("{est=") + 5, this.props.description.indexOf("=est}") ) : "TBD",
-        scheduled: (this.props.description.indexOf("{sch=") > -1) ?
-            Moment(
-                this.props.description.substring( this.props.description.indexOf("{sch=") + 5,  this.props.description.indexOf("{sch=") + 5 + 17 ))
-                .format("ddd M/D h:mma") : "TBD",
-        scheduledDate: (this.props.description.indexOf("{sch=") > -1) ?
-            new Date(this.props.description.substring( this.props.description.indexOf("{sch=") + 5,  this.props.description.indexOf("{sch=") + 5 + 17 ))
-                : null,
-        description: this.props.description.replace(/\{[^}]+\}/g, '').replace(/(^[ \t]*\n)/gm, ""),
+	constructor(props) {
+		super();
+		this.state = {
+	        cycle: Math.floor(Math.random()*60)+"min",
+	        due: props.due,
+	        duePretty: (props.due) ? Moment(props.due).format("ddd M/D h:mma") : "TBD",
+	        estimated: (props.description.indexOf("{est=") > -1) ?
+	            props.description.substring( props.description.indexOf("{est=") + 5, props.description.indexOf("=est}") ) : "TBD",
+	        scheduled: (props.description.indexOf("{sch=") > -1) ?
+	            Moment(
+	                props.description.substring( props.description.indexOf("{sch=") + 5,  props.description.indexOf("{sch=") + 5 + 20 ))
+	                .format("ddd M/D h:mma") : "TBD",
+	        scheduledStart: (props.description.indexOf("{sch=") > -1) ?
+	            Moment(props.description.substring( props.description.indexOf("{sch=") + 5,  props.description.indexOf("{sch=") + 5 + 20 ))
+	                : null,
+			scheduledEnd: (props.description.indexOf("=sch}") > -1) ?
+	            Moment(props.description.substring( props.description.indexOf("=sch}") - 20,  props.description.indexOf("=sch}") ))
+	                : null,
+	        description: (props.description) ? props.description.replace(/\{[^}]+\}/g, '').replace(/(^[ \t]*\n)/gm, "") : "",
+	        editing: false,
+	        title: props.title,
+	        subtitle: props.subtitle
+    	}
+	    this.edit = this.edit.bind(this);
+	    this.cancelEdit = this.cancelEdit.bind(this);
+	    this.updateCard = this.updateCard.bind(this);
+	}
+
+    edit() {
+    	this.setState({editing: true});
     }
 
-    componentDidMount() {
-        
+    updateCard(e) {
+    	//var self=this;
+    	e.preventDefault();
+
+    	var form = document.forms["edit-"+this.props.id];
+    	var utcOffset = (new Date().getTimezoneOffset() / 60);
+
+    	var scheduled = "";
+    	var estimated = "";
+
+    	if(form.scheduledDate.value !== "" && //"2017-08-27"
+			form.scheduledTimeStart.value !== "" &&
+			form.scheduledTimeEnd.value !== "") {
+
+    		var start = Moment(form.scheduledDate.value+" "+form.scheduledTimeStart.value).add(utcOffset, 'hours');
+    		var end = Moment(form.scheduledDate.value+" "+form.scheduledTimeEnd.value).add(utcOffset, 'hours');
+
+    		scheduled = "\n{sch=" + Moment(start).format("YYYY-MM-DDTHH:mm:00[Z]") + "|" + Moment(end).format("YYYY-MM-DDTHH:mm:00[Z]") + "=sch}";
+    	}
+
+    	if(form.estimated.value !== "" && form.estimated.value !== "TBD") {
+    		estimated = "\n{est=" + form.estimated.value + "=est}";
+    	}
+
+    	var due = null;
+    
+		if(form.dueDate.value !== "" &&
+			form.dueTime.value !== "") {
+
+    		due = Moment(form.dueDate.value+" "+form.dueTime.value);//.add(utcOffset, 'hours');
+    		//due = Moment(due).format("YYYY-MM-DDTHH:mm:00[Z]");
+    	}
+
+    	var params = {
+    		key: Authentication.TrelloKey,
+    		token: Authentication.TrelloToken,
+    		name: form.name.value,
+    		desc: form.desc.value + scheduled + estimated,
+    		due: due.toDate()
+    	}
+
+    	jQuery.ajax({
+    		type: 'PUT',
+    		url: "https://api.trello.com/1/cards/"+this.props.id,
+    		data: params,
+    		success: function() {
+    			window.location.reload();
+//    			self.props.poll();
+
+    			/*self.setState({
+    				title: form.name.value,
+    				estimated: form.estimated.value,
+    				due: due,
+    				editing: false,
+    				duePretty: (due) ? Moment(due).format("ddd M/D h:mma") : "TBD"
+    			});*/
+    		},
+    		error: function(xhr) {
+    			alert("Error saving your changes: "+xhr.responseText);
+    		}
+    	})
+
+    	
+
+    	console.log(params);
+    }
+
+    cancelEdit() {
+    	this.setState({editing: false});
     }
 
     render() {
-        return (
-            
-            <div className="card draggable card-outline-info"> 
-              <div className="card-block">
+    	if(this.state.editing)
+    	{
+    		return ( 
 
-                <h4 className="card-title">
-                    {this.props.title}
-                    <span className="card-priority">
-                        <Icon name="pencil" />
-                    </span>
-                </h4>
+    			<form id={"edit-"+this.props.id} onSubmit={this.updateCard}>
+	    			<div className="card card-outline-info"> 
+		              <div className="card-block">
 
-                <h6 className="card-subtitle mb-2 text-muted">
-                    {this.props.subtitle}
-                </h6>
+		                <h4 className="card-title">
+		                    <input name="name" type="text" placeholder="Title" defaultValue={this.state.title} />
+		                </h4>
 
-                <p className="card-text">
-                    {this.state.description}
-                </p>
+		                <h6 className="card-subtitle mb-2 text-muted">
+		                    <select name="label" defaultValue={this.state.subtitle}><option value={this.state.subtitle}>{this.state.subtitle}</option></select>
+		                </h6>
 
-                <Stat icon="clock-o" value={this.state.estimated} />
-                <Stat icon="calendar" value={this.state.scheduled} scheduled={this.state.scheduledDate} /><br />
-                  {/*<Stat icon="refresh" value={this.state.cycle}*/}
-                <Stat icon="inbox" value={this.state.due} due={this.props.due} />
+		                <p className="card-text">
+		                    <Textarea name="desc" placeholder="Description" defaultValue={this.state.description} />
+		                </p>
 
-              </div>
-            </div>
+		                <Stat icon="clock-o" type="text" value={this.state.estimated} editing />
+		                <Stat icon="calendar" type="event" value={this.state.scheduled} scheduledStart={this.state.scheduledStart} scheduledEnd={this.state.scheduledEnd} editing />
+		                  {/*<Stat icon="refresh" value={this.state.cycle}*/}
+		                <Stat icon="inbox" type="date-time" value={this.state.duePretty} due={this.state.due} editing />
+		                
+		                <input className="btn btn-success" type="submit" value="&#10004;" />
+		                <input className="btn btn-danger" type="button" value="X" onClick={this.cancelEdit} />
 
-        );
+		              </div>
+		            </div>
+		        </form>
+
+	       );
+    	}
+    	else
+    	{
+	        return (
+	            
+	            <div className="card card-outline-info"> 
+	              <div className="card-block">
+
+	                <h4 className="card-title">
+	                    {this.state.title}
+	                    <span className="card-priority" onClick={this.edit}>
+	                        <Icon name="pencil" />
+	                    </span>
+	                </h4>
+
+	                <h6 className="card-subtitle mb-2 text-muted">
+	                    {this.state.subtitle}
+	                </h6>
+
+	                <p className="card-text">
+	                    {this.state.description}
+	                </p>
+
+	                <Stat icon="clock-o" value={this.state.estimated} />
+	                <Stat icon="calendar" value={this.state.scheduled} scheduled={this.state.scheduledDate} /><br />
+	                  {/*<Stat icon="refresh" value={this.state.cycle}*/}
+	                <Stat icon="inbox" value={this.state.duePretty} due={this.state.due} />
+
+	              </div>
+	            </div>
+
+	        );
+	    }
     }
 
 }
@@ -143,6 +318,8 @@ class Column extends Component {
                         description={card.desc}
                         due={card.due}
                         key={card.id}
+                        id={card.id}
+                        poll={this.props.poll}
                     />
                 ))}
                
@@ -153,22 +330,21 @@ class Column extends Component {
 
 class App extends Component {
 
-    state = {
-        columns: [
-            { title: "Open", id: "598fa1bee1a7e2a5587befc3" },
-            { title: "Scheduled", id:"598fa1c7ddb0ea0cc4c5773e" },
-            { title: "In Progress", id: "598fa2fe443be451928c95d3" },
-            { title: "On Hold", id: "598fa30575e5b628c02ea25b" },
-            { title: "Done", id: "598fa308e4d27b60e4f31afd" }
-        ],
-        
-        cards: [ /*{
-            title: "Loading...",
-            idList: "598fa2fe443be451928c95d3",
-            labels: [],
-            desc: ""
-        }*/]
-    }
+	constructor(props) {
+		super();
+		this.state = {
+	        columns: [
+	            { title: "Open", id: "598fa1bee1a7e2a5587befc3" },
+	            { title: "Scheduled", id:"598fa1c7ddb0ea0cc4c5773e" },
+	            { title: "In Progress", id: "598fa2fe443be451928c95d3" },
+	            { title: "On Hold", id: "598fa30575e5b628c02ea25b" },
+	            { title: "Done", id: "598fa308e4d27b60e4f31afd" }
+	        ],
+	        cards: []
+	    }
+	}
+
+    
 
     componentDidMount() {
         this.startPolling();
@@ -206,7 +382,7 @@ class App extends Component {
                 <div className="container-fluid" id="board">
                     <div className="row">
                         {this.state.columns.map(column => (
-                            <Column title={column.title} className={column.title} key={column.id} id={column.id} cards={this.state.cards} />
+                            <Column title={column.title} className={column.title} key={column.id} id={column.id} cards={this.state.cards} poll={this.poll} />
                         ))}
                     </div>
                 </div>
